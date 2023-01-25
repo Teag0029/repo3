9b7bb8ef0505eff83995edb8d731ef1969eaaa50
alyxx2214 <jonathan.a.teague-1@ou.edu>
2023-01-25 14:55:13 -0600

    Post following directions
    Revised instructions a little. I'll do one more test, then call it good for today.

--- Date_Year_Filename(Repo3)/src/testPackage/TestPackage.java
+++ Date_Year_Filename(Repo3)/src/testPackage/TestPackage.java
@@ -10,46 +10,51 @@ public class TestPackage {
 	}
 
 }
+
 /*
- * Steps to import git repo AND create new project:
- *
- *1. have a repo ready to clone
- *- clone repo into a folder, and keep track of its FILEPATH
- *
- *2. open Eclipse in your Workspace
- *- use the working space of Coolb/Documents/GitHub on desktop
- *- use the working space of faijwefiuu/GitHub on laptop
- *
- *3. Create a new Java Project, titled "MonthDay_Year_Filename"
- *- month is usually 3-letter abbr
- *- year is usually 4 digits
- *- filename is usually project-related
- *
- *4. open files
- *- navigate to the GitHub folder
- *
- *5. Move project folder from GitHub to repo FILEPATH
- *
- *6. go back to eclipse
- *
- *7. File > impoprt
- *
- *8. Git > Projects from Git
- *
- *9. Existing local repository
- *
- *10. Add
- *
- *11. Browse to GitHub folder
- *
- *12. select the git project you'd like to later import > Add
- *
- *13. select the repo > Next
- *
- *14. Import existing Eclipse projects > Next > select the project to import > Finish
- *
- *15. continue like you would. all changes sync with the Github repo. 
- *As long as you commit and push, then your work gets upploaded too
- */
+
+Steps to import git repo AND create new project:
+
+1. have a repo ready to clone
+- clone repo into a folder, and keep track of its FILEPATH
+
+2. open Eclipse in your Workspace
+- use the working space of Coolb/Documents/GitHub on desktop
+- use the working space of faijwefiuu/GitHub on laptop
+
+3. Create a new Java Project, titled "MonthDay_Year_Filename"
+- month is usually 3-letter abbr
+- year is usually 4 digits
+- filename is usually project-related
+
+4. open files
+- navigate to the GitHub folder
+
+5. Move project folder from GitHub to repo FILEPATH
+
+6. go back to eclipse
+
+7. delete project from **JUST** the workspace
+- ==DO NOT DELETE FROM DISK==
+- **will probably throw 'file not found' error > hit 'continue'**
+
+7. File > import
+
+8. Git > Projects from Git
+
+9. Existing local repository
+
+10. If repo not already added > Add
+a) Browse to GitHub folder
+b) select the repos u wanna pull from > Add
+
+11. select the repo u wanna pull from > Next
+
+12. Import existing Eclipse projects > Next > select the project(s) to import > Finish
+
+15. continue like you would. all changes sync with the Github repo. 
+As long as you commit and push, then your work gets upploaded too
+
+*/
 
 
---
+++ Jan25_2023_TestProjectUsingInstructions/src/testPackage/TestClass.java
@@ -0,0 +1,12 @@
+package testPackage;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("say something, TestClass!!!");
+		//yay!! it worked
+		//it even uploads to github!
+	}
+
+}
