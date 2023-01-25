ea62c13346f2bf2243214b814faa6bd1defbe9b0
alyxx2214 <jonathan.a.teague-1@ou.edu>
2023-01-25 15:00:28 -0600

    Final Test Done for the day
    Yep, the revised instructions work perfectly!
    I think I'll continue my ZyBooks;
    Might even start a repo for it

---
+++ Jan25_2023_FinalTest_RevisedInstructions/src/testPackage/TestClass.java
@@ -0,0 +1,12 @@
+package testPackage;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welp, time to see if I was right");
+		System.out.println("Yep, she prints!");
+		//and updates git!
+	}
+
+}
